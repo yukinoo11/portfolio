9060254b12bb56f0e56fe66187451c2de7e2f390
쩡 <yukinoo11@naver.com>
2021-08-02 16:56:29 +0900

    Signed-off-by: 쩡 <yukinoo11@naver.com>

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import MetaTags from 'react-meta-tags';
+import AdminApp from './admin/Index';
+import MainApp from './main/index';
+import WebSite from './webSite/WebSite';
+import SignIn from './admin/SignIn';
+import Infomation from "./portfolio/Information";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import RestrictRoute  from "./api/component/RestricRoute";
+
+class App extends Component {
+  render(){
+    return(
+      <>
+        <MetaTags>
+          <meta httpEquiv="origin-trial" content="TOKEN_GOES_HERE" />
+          <meta name = 'theme-color' content='#333333'/>
+          <meta name = 'viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1, minimum-scale=1.0, user-scalable=no' />
+        </MetaTags>
+        
+        <Router>
+            <Switch>
+              <Route exact path='/yukinoo11Main' component = { MainApp } />
+              <RestrictRoute component={AdminApp} path="/admin" exact />
+              <Route exact path='/signin' component = { SignIn } />
+              <Route path='/admin/:id' component = { AdminApp } />
+              
+              {/*
+              <Route path='/study' component = { StudyMain } />
+               */ }
+              <Route path='/' component={ WebSite } />
+              {/*
+              <Route exact path='/game' component={ GameApp } />
+              <Route exact path='/play/:id' component={ GamePlay } />
+              */ }
+              <Route component = { NotFound } />
+            </Switch>
+        </Router>
+      </>
+    )
+  }
+}
+export default App;
+
+const NotFound = ( props ) => {
+  const { match } = props;
+
+  return(
+    <div className='NotFound'>
+      <main>
+        <span className='message'>요청한 페이지를 찾을수 없습니다.</span>
+        <span className='subMessage'> Path:{ match.path }  , URL : {match.url}</span>
+        <span><Link to='./' className='button-color'>이전 페이지로</Link></span>
+      </main>
+    </div>
+  )
+}
